da5fd0b4a525d3289f51fc90bf85cc9a85d354cd
farhansyihab <agiptek@gmail.com>
2026-01-03 05:04:21 +0700

    halaman admin yang lebih responsif

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,23 @@
+import os
 from flask import Flask
-from .config import Config
+from werkzeug.middleware.proxy_fix import ProxyFix
+from .config import Config, ProductionConfig
 from .services.oauth import init_oauth
 
 def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
+    env = os.getenv("FLASK_ENV", "development")
 
-    # init oauth
+    app = Flask(__name__)
+
+    if env == "production":
+        app.config.from_object(ProductionConfig)
+        app.config["PREFERRED_URL_SCHEME"] = "https"
+        app.config["SERVER_NAME"] = os.getenv("SERVER_NAME")
+    else:
+        app.config.from_object(Config)
+
     init_oauth(app)
 
-    # register blueprints
     from .auth.routes import auth_bp
     from .admin.routes import admin_bp
     from .public.routes import public_bp
@@ -17,5 +25,13 @@ def create_app():
     app.register_blueprint(auth_bp, url_prefix="/auth")
     app.register_blueprint(admin_bp, url_prefix="/admin")
     app.register_blueprint(public_bp)
+
+    app.wsgi_app = ProxyFix(
+        app.wsgi_app,
+        x_for=1,
+        x_proto=1,
+        x_host=1,
+        x_port=1,
+    )
 
     return app
--- app/config.py
+++ app/config.py
@@ -4,9 +4,16 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
     ENV = os.getenv("FLASK_ENV", "development")
 
-    # OAuth (nanti dipakai)
     GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
     GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
 
-    # Firestore
     GCP_PROJECT = os.getenv("GCP_PROJECT")
+
+    # DEFAULT (LOCAL + DOCKER)
+    SESSION_COOKIE_SECURE = False
+    SESSION_COOKIE_SAMESITE = "Lax"
+
+
+class ProductionConfig(Config):
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_SAMESITE = "None"
