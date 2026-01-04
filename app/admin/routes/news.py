2bf02483b0dfe4e0bac15b23151f3d2725aa66a7
farhansyihab <agiptek@gmail.com>
2026-01-04 08:05:45 +0700

    fix edit article

--- app/admin/routes/news.py
+++ app/admin/routes/news.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, url_for, flash, g
 from datetime import datetime
 import traceback
 
